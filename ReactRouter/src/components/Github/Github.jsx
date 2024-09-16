60cb2024b0556de483820b6794d0ae57cfeb472c
Archana-PC <archanamikki1998@gmail.com>
2024-09-16 19:40:17 +0530

    add all file

---
+++ ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github()
+{
+
+    const data = useLoaderData() 
+    // const [data, setData] = useState([])
+
+    // useEffect(() => {
+    // fetch('https://github.com/hiteshchoudhary/')
+    // .then(res => res.json())
+    // .then(data => setData(data))
+  
+    //  },[])
+  return (
+    <div className='w-full h-screen bg-gray-200 auto text-center  text-white'>Github Followers:{data.followers}
+    </div>
+  )
+}
+
+export default Github
+
+export const githubLoder = async () => {
+    const res =await(fetch('https://github.com/hiteshchoudhary/'))
+    return res.json() 
+}
